593d92cd4ec5b9fbd9c5841220553aa12dc0f8e6
Florian Kraft <florian.kraft@contentful.com>
2021-03-01 20:49:03 +0100

    basic docs and license

--- mix.exs
+++ mix.exs
@@ -6,8 +6,15 @@ defmodule Mimicry.MixProject do
       app: :mimicry,
       version: "0.0.0",
       elixir: "~> 1.10",
+      description: description(),
+      package: package(),
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+
+      # Docs
+      docs: [
+        main: "Mimicry"
+      ]
     ]
   end
 
@@ -21,8 +28,34 @@ defmodule Mimicry.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
+      {:ex_doc, "~> 0.21", only: :dev, runtime: false}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+    ]
+  end
+
+  defp description do
+    """
+    A small server to generate on demand mock servers based on a specification
+    """
+  end
+
+  defp package do
+    [
+      name: :mimicry,
+      files: [
+        "lib/",
+        "mix.exs",
+        "README*",
+        "LICENSE*"
+      ],
+      maintainers: [
+        "Florian Kraft"
+      ],
+      licenses: ["MIT"],
+      links: %{
+        "GitHub" => "https://github.com/floriank/mimicry"
+      }
     ]
   end
 end
