c036949c6c853aa59a370df96529292aa7075495
Florian Kraft <florian.kraft@contentful.com>
2021-03-18 09:35:50 +0100

    move overrides down

--- config/config.exs
+++ config/config.exs
@@ -13,10 +13,6 @@ config :logger, :console,
   format: "$time $metadata[$level] $message\n",
   metadata: [:request_id]
 
-# Import environment specific config. This must remain at the bottom
-# of this file so it overrides the configuration defined above.
-import_config "#{Mix.env()}.exs"
-
 # Configures the endpoint
 config :mimicry, MimicryApi.Endpoint,
   url: [host: "localhost"],
@@ -25,3 +21,7 @@ config :mimicry, MimicryApi.Endpoint,
     view: MimicryApi.ErrorView,
     accepts: ~w(json)
   ]
+
+# Import environment specific config. This must remain at the bottom
+# of this file so it overrides the configuration defined above.
+import_config "#{Mix.env()}.exs"
