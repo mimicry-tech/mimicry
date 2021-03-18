ae6282451d39d7f136a021e1e4df1b351da6db2c
Florian Kraft <florian.kraft@contentful.com>
2021-03-18 09:33:59 +0100

    updates deps

--- mix.exs
+++ mix.exs
@@ -6,10 +6,12 @@ defmodule Mimicry.MixProject do
       app: :mimicry,
       version: "0.0.0",
       elixir: "~> 1.10",
+      elixirc_paths: elixirc_paths(Mix.env()),
       description: description(),
       package: package(),
       start_permanent: Mix.env() == :prod,
       deps: deps(),
+      aliases: aliases(),
 
       # Docs
       docs: [
@@ -21,18 +23,32 @@ defmodule Mimicry.MixProject do
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger]
+      mod: {Mimicry.Application, []},
+      extra_applications: [:logger, :runtime_tools]
     ]
   end
 
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:ex_doc, "~> 0.21", only: :dev, runtime: false}
+      {:jason, "~> 1.0"},
+      {:phoenix, "~> 1.5"},
+      {:plug_cowboy, "~> 2.4"},
+
+      # dev
+      {:ex_doc, "~> 0.21", only: :dev, runtime: false},
+      {:mix_test_watch, "~> 1.0", only: :dev, runtime: false}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
   end
+
+  # Specifies which paths to compile per environment.
+  defp elixirc_paths(:test), do: ["lib", "test/support"]
+  defp elixirc_paths(:dev), do: ["lib", "test/support"]
+  defp elixirc_paths(_), do: ["lib"]
+
+  def aliases, do: []
 
   defp description do
     """
