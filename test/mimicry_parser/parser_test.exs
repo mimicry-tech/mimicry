692260e68b82a65241fcc6766e869cdd9db6967f
Florian Kraft <florian.kraft@contentful.com>
2021-03-19 16:23:24 +0100

    tests for yaml and json

--- test/mimicry_parser/parser_test.exs
+++ test/mimicry_parser/parser_test.exs
@@ -4,13 +4,29 @@ defmodule MimicryParser.ParserTest do
   alias MimicryParser.Parser
   alias Mimicry.OpenAPI.Specification
 
-  describe "parse/1" do
+  describe "read/1" do
     test "reads a given file" do
       {:ok, %Specification{original_file: _}} = Parser.read("simple.yaml")
     end
 
     test "errors when file does not exist" do
       {:error, :enoent} = Parser.read("non_existing.yaml")
+    end
+  end
+
+  describe "parse/1" do
+    test "reads a JSON file" do
+      {:ok, file} = Parser.read("simple.json")
+      spec = file |> Parser.parse()
+
+      assert(spec["openapi"] == "3.0.0")
+    end
+
+    test "reads a YAML file" do
+      {:ok, file} = Parser.read("simple.yaml")
+      spec = file |> Parser.parse()
+
+      assert(spec["openapi"] == "3.0.0")
     end
   end
 end
