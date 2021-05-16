f1034ec1255f35d109df77374ddbe1a1161164c8
Florian Kraft <florian.kraft@contentful.com>
2021-05-16 14:16:35 +0200

    adapts configuration approach from livebook

---
+++ lib/mimicry/config.ex
@@ -0,0 +1,47 @@
+defmodule Mimicry.Config do
+  @moduledoc """
+  Provides functions around configuration for `Mimicry.Application`
+  """
+
+  @doc """
+  Parses and validates the ip from env.
+  """
+  def ip!(env) do
+    if ip = System.get_env(env) do
+      ip!(env, ip)
+    end
+  end
+
+  @doc """
+  Parses and validates the ip within context.
+  """
+  def ip!(context, ip) do
+    case ip |> String.to_charlist() |> :inet.parse_address() do
+      {:ok, ip} ->
+        ip
+
+      {:error, :einval} ->
+        abort!("expected #{context} to be a valid ipv4 or ipv6 address, got: #{ip}")
+    end
+  end
+
+  @doc """
+  Parses and validates the port from env.
+  """
+  def port!(env) do
+    if port = System.get_env(env) do
+      case Integer.parse(port) do
+        {port, ""} -> port
+        :error -> abort!("expected #{env} to be an integer, got: #{inspect(port)}")
+      end
+    end
+  end
+
+  @doc """
+  Aborts booting due to a configuration error.
+  """
+  def abort!(message) do
+    IO.puts("\nERROR!!! [Mimicry] " <> message)
+    System.halt(1)
+  end
+end
