62a7ccac2d48683cf8cd01f626fd2e3ffc43a751
Florian Kraft <florian.kraft@contentful.com>
2021-09-13 09:42:28 +0200

    fixes style
    ...or: how I figured out my editor at home is misconfigured

--- lib/mimicry_api/errors.ex
+++ lib/mimicry_api/errors.ex
@@ -1,5 +1,8 @@
 defmodule MimicryApi.Errors do
-	def make_errors_from_openapi_validation(errors) do
-		errors |> Enum.map(fn { msg, path } -> %{ path => msg} end)
-	end
+  @moduledoc """
+  Provides functions for transforming errors from libs into something usable to the API
+  """
+  def make_errors_from_openapi_validation(errors) do
+    errors |> Enum.map(fn {msg, path} -> %{path => msg} end)
+  end
 end
